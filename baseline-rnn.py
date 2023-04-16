04565f7b98a424d2d4234daa07a1cbc512744029
Sammy Franklin <sammyfranklin@yahoo.com>
2023-04-15 19:20:55 -0700

    first iteration of rnn

--- baseline-rnn.py
+++ baseline-rnn.py
@@ -1,5 +1,3 @@
-import os.path as osp
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -10,26 +8,39 @@ from torch_geometric.loader import DataLoader
 class BaselineRNN(nn.Module):
     def __init__(self):
         super(BaselineRNN, self).__init__()
-        self.rnn = nn.RNN(input_size=1, hidden_size=1, num_layers=1, batch_first=True)
+        self.num_layers = 4
+        self.hidden_size = 8
+        self.rnn = nn.RNN(input_size=1, hidden_size=self.hidden_size, num_layers=self.num_layers)
+        self.linear = nn.Linear(self.hidden_size, 1)
 
-    def forward(self, data):
-        h = 0
-        for i in range(data.edge_index.shape[1]):
-            x = data.edge_index[0][i]
-            _, h = self.rnn(x, h)
-        x = data.edge_index[1][-1]
-        out, _ = self.rnn(x, h)
-        return F.log_softmax(out, dim=1)
+    def get_initial_h(self, batch_size):
+        size = (self.num_layers, batch_size, self.hidden_size)
+        return torch.zeros(size, device=device, dtype=torch.float)
 
+    def forward(self, x, initial_h):
+        out, _ = self.rnn(x, initial_h)
+        out = nn.utils.rnn.unpack_sequence(out)
+        final_states = []
+        for seq in out:
+            final_state = seq[-1]
+            final_states.append(final_state)
+        out = torch.stack(final_states)
+        out = self.linear(out)
+        return out
+
+batch_size = 32
 dataset = SessionsDataset('./data')
-loader = DataLoader(dataset, batch_size=32, shuffle=True)
+loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = BaselineRNN().to(device)
+loss_fn = nn.MSELoss()
+model.train()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
 
 print(dataset.num_classes)
 print(dataset.num_node_features)
 print(dataset[0])
+print(dataset[0].edge_index)
 
 def test():
     model.eval()
@@ -40,22 +51,44 @@ def test():
         accs.append(acc)
     return accs
 
-
-def train_step(batch):
-    model.train()
+def train_step(data, y):
+    data = data.to(device)
+    y = y.to(device)
     optimizer.zero_grad()
-    F.nll_loss(model()[data.train_mask], data.y[data.train_mask]).backward()
+    out = model(data, model.get_initial_h(batch_size))
+    loss = loss_fn(out, y)
+    loss.backward()
     optimizer.step()
+    return loss.item()
 
+def get_packed_seq_from_batch(batch):
+    batch_indices = batch.batch
+    samples = [[] for _ in range(batch_size)]
+    for i,idx in enumerate(batch_indices):
+        samples[idx].append(batch.x[i])
+    for i in range(len(samples)):
+        if len(samples[i]) == 0:
+            return None
+        samples[i] = torch.stack(samples[i]).to(dtype=torch.float)
+    samples = nn.utils.rnn.pack_sequence(samples, enforce_sorted=False)
+    return samples
+
+step = 0
 best_val_acc = test_acc = 0
 for epoch in range(1,100):
-    for data in loader:
-        train_step()
-    _, val_acc, tmp_test_acc = test()
-    if val_acc > best_val_acc:
-        best_val_acc = val_acc
-        test_acc = tmp_test_acc
-    log = 'Epoch: {:03d}, Val: {:.4f}, Test: {:.4f}'
+    for batch in loader:
+        data = get_packed_seq_from_batch(batch)
+        if data == None:
+            continue
+        loss = train_step(data, batch.y)
+        step += 1
+        if step % 100 == 0:
+            print(loss)
+    # _, val_acc, tmp_test_acc = test()
+    # if val_acc > best_val_acc:
+    #     best_val_acc = val_acc
+    #     test_acc = tmp_test_acc
+    # log = 'Epoch: {:03d}, Val: {:.4f}, Test: {:.4f}'
     
-    if epoch % 10 == 0:
-        print(log.format(epoch, best_val_acc, test_acc))
+    # if epoch % 10 == 0:
+    #     print(log.format(epoch, best_val_acc, test_acc))
--- data.py
+++ data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import torch
+import math
 import os.path as osp
 from torch_geometric.data import Dataset, Data
 from torch_geometric.loader import DataLoader
@@ -8,17 +9,24 @@ from ast import literal_eval
 
 class SessionsDataset(Dataset):
   def __init__(self, root, transform=None, pre_transform=None, pre_filter=None):
-    super().__init__(root, transform, pre_transform, pre_filter)
     # self.processed_path = osp.join(self.root, "processed.pt")
     # self.data, self.slices = torch.load(self.processed_paths[0])
 
     sessions_path = "sessions_train.csv"
     nodes_path = "products_train.csv"
-    self.sessions = pd.read_csv(osp.join(self.root, sessions_path))
-    self.nodes = pd.read_csv(osp.join(self.root, nodes_path))
+    self.sessions = pd.read_csv(osp.join(root, sessions_path))
+    self.nodes = pd.read_csv(osp.join(root, nodes_path))
     self.id_mapping = {id: i for i, id in enumerate(self.nodes['id'])}
     self.sessions['prev_items'] = self.sessions['prev_items'].replace(' ', ',', regex=True)
     self.sessions['prev_items'] = self.sessions['prev_items'].apply(literal_eval)
+    n = len(self.sessions.index)
+    mean = (n + 1) / 2
+    var = (n+1)*(2*n+1)/6 - ((n+1)**2)/4
+    std = math.sqrt(var)
+    print(mean, std)
+    self.mean, self.std = mean, std
+    self.data_list = None
+    super().__init__(root, transform, pre_transform, pre_filter)
 
   @property
   def raw_file_names(self):
@@ -28,39 +36,37 @@ class SessionsDataset(Dataset):
   def processed_file_names(self):
     return [f'data.pt']
 
-  # def process(self):
-  #   pass
-  #   # sessions_path = "sessions_train.csv"
-  #   # nodes_path = "products_train.csv"
-  #   # sessions = pd.read_csv(osp.join(self.root, sessions_path))
-  #   # nodes = pd.read_csv(osp.join(self.root, nodes_path))
-  #   # id_mapping = {id: i for i, id in enumerate(nodes['id'])}
+  def process(self):
+    # sessions_path = "sessions_train.csv"
+    # nodes_path = "products_train.csv"
+    # sessions = pd.read_csv(osp.join(self.root, sessions_path))
+    # nodes = pd.read_csv(osp.join(self.root, nodes_path))
+    # id_mapping = {id: i for i, id in enumerate(nodes['id'])}
+    # sessions['prev_items'] = sessions['prev_items'].replace(' ', ',', regex=True)
+    # sessions['prev_items'] = sessions['prev_items'].apply(literal_eval)
+    data_list = []
+    i = 0
+    n = self.len()
+    for _, row in self.sessions.iterrows():
+      # print(f'{i} / {n}')
+      if i == n:
+        break
+      codes, uniques = pd.factorize(row['prev_items'])
+      x = [self.id_mapping[id] for id in uniques]
+      x = torch.tensor(x, dtype=torch.float).unsqueeze(1)
+      x = (x - self.mean) / self.std
+      edge_index = torch.tensor(np.array([ codes[:-1], codes[1:] ]), dtype=torch.long)
+      y = self.id_mapping[row['next_item']]
+      y = torch.tensor([y], dtype=torch.float)
+      y = (y - self.mean) / self.std
+      data_list.append( Data(x=x, edge_index=edge_index, y=y) )
+      i += 1
 
-  #   # sessions['prev_items'] = sessions['prev_items'].replace(' ', ',', regex=True)
-  #   # sessions['prev_items'] = sessions['prev_items'].apply(literal_eval)
-  #   # data_list = []
-  #   # for _, row in sessions.iterrows():
-  #   #   codes, uniques = pd.factorize(row['prev_items'])
-  #   #   x = [id_mapping[id] for id in uniques]
-  #   #   x = torch.tensor(x, dtype=torch.long).unsqueeze(1)
-  #   #   edge_index = torch.tensor(np.array([ codes[:-1], codes[1:] ]), dtype=torch.long)
-  #   #   y = id_mapping[row['next_item']]
-  #   #   y = torch.tensor(y, dtype=torch.long)
-  #   #   data_list.append( Data(x=x, edge_index=edge_index, y=y) )
-
-    # data, slices = self.collate(data_list)
-    # torch.save((data, slices), self.processed_paths[0])
+    torch.save(data_list, self.processed_paths[0])
   def len(self):
-    return len(self.sessions.index)
+    return len(self.sessions.index) // 1000
   
   def get(self, idx):
-    row = self.sessions.iloc[idx]
-    codes, uniques = pd.factorize(row['prev_items'])
-    x = [self.id_mapping[id] for id in uniques]
-    x = torch.tensor(x, dtype=torch.long).unsqueeze(1)
-    edge_index = torch.tensor(np.array([ codes[:-1], codes[1:] ]), dtype=torch.long)
-    y = self.id_mapping[row['next_item']]
-    y = torch.tensor(y, dtype=torch.long)
-    return Data(x=x, edge_index=edge_index, y=y)
-    # data = torch.load(osp.join(self.processed_dir, f'data_{idx}.pt'))
-    return data
+    if self.data_list == None:
+      self.data_list = torch.load(self.processed_paths[0])
+    return self.data_list[idx]
